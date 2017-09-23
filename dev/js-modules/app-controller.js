bcd4f4a2421bf336b0900bcb641b15000c7983f2
Margo <luckinamargo@gmail.com>
2017-09-23 12:34:47 +0700

    add prices

--- dev/js-modules/app-controller.js
+++ dev/js-modules/app-controller.js
@@ -3,6 +3,10 @@ angular.module('app')
         let self = this;
         self.data = undefined;
 
+        this.capitalizeFirstLetter = function (string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        };
+
         $http({method: 'GET', url: 'https://rest.vsemayki.ru/catalog/items?access-token=f6ed359e9eab534b09d846ed56e6e196e7295076'})
             .then(function success(response) {
                 self.data = response.data;
